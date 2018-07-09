70adfb1d73a460099aef5ef66b1cbd3917441996
malamsyah <mochammadalamsyah24@gmail.com>
2018-07-09 16:37:30 +0700

    add get containers api

--- main.go
+++ main.go
@@ -8,7 +8,19 @@ import (
 	lxd "github.com/lxc/lxd/client"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func getResourceHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		response, err := json.Marshal(`{"message":"request not found"}`)
+		if err != nil {
+			panic(err)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(response)
+		return
+	}
+
 	c, err := lxd.ConnectLXDUnix("", nil)
 	if err != nil {
 		panic(err)
@@ -27,13 +39,44 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(response)
+
+}
+
+func getContainersHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		returnJSON(w, `{"message":"request not found"}`)
+		return
+	}
+
+	c, err := lxd.ConnectLXDUnix("", nil)
+	if err != nil {
+		panic(err)
+	}
+
+	containers, err := c.GetContainers()
+	if err != nil {
+		panic(err)
+	}
+
+	returnJSON(w, containers)
 }
 
 func main() {
-
-	http.HandleFunc("/lxd/status", handler)
+	http.HandleFunc("/lxd/resource", getResourceHandler)
+	http.HandleFunc("/lxd/containers", getContainersHandler)
 	err := http.ListenAndServe(":4041", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
+}
+
+func returnJSON(w http.ResponseWriter, data interface{}) {
+	response, err := json.Marshal(data)
+	if err != nil {
+		panic(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
 }
